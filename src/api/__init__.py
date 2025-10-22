27c2b9158608e79e37aec8fb02cdc3fb31fb23cb
Gabor <gabor@w7-7.net>
2025-10-22 01:42:09 +0000

    Revert "Merge pull request #3 from w7-mgfcode/master"
    This reverts commit 7a9c23bdcd42a6e0baffd5dbb069ea0c9ef9b51f, reversing
    changes made to 9b407ce97eb099710f70fdac3f3919c43208df09.

--- src/api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-REST API module for Safe Auto-Updater.
-"""
-
-from .server import create_app, start_server
-
-__all__ = ['create_app', 'start_server']
--- src/monitoring/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Monitoring and metrics module.
-"""
-
-from .prometheus_metrics import MetricsCollector
-
-__all__ = ['MetricsCollector']
